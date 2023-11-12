0d76ddf65360a65414ddb41c98ada874f0bd7a2b
Owando <50991080+ozendorf@users.noreply.github.com>
2023-11-12 23:35:34 +0100

    migrating to yt-dlp

--- stt.sh
+++ stt.sh
@@ -3,7 +3,7 @@ video_url=$1
 video_id=$(( RANDOM % 10000 ))
 video_path="video$video_id.mp4"
 
-youtube-dl $1 -o $video_path
+yt-dlp -f mp4 $1 -o $video_path
 audio_path="audio$video_id.wav"
 ffmpeg -i $video_path $audio_path
-python speechtotext.py "./$video_path" "./$audio_path"
+python speechtotext.py "./$video_path" "$audio_path"
